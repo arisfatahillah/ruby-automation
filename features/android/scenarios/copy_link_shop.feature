73455dcb964f615d63399e26172262a356441abe
aris nugraha <aris@sampingan.co.id>
2022-07-04 13:25:01 +0700

    add scenario for android

---
+++ features/android/scenarios/copy_link_shop.feature
@@ -0,0 +1,15 @@
+@homepage
+Feature: Evermos - Homepage
+  
+  Scenario: User can go to my shop landing page
+    Given user in onboarding screen
+    And user click button name "Saya Sudah Punya Akun"
+    And validate text "Belum jadi reseller?" exist on page
+    When user input phone number "6281223334444" on login page
+    And user input password "password" on login page
+    And user click button name "Masuk"
+    And validate text "Yuk, ikuti panduan berjualan sebelum mulai berikhtiar." exist on page
+    And user click button name "Nanti Saja"
+    When user click text view "Akun Saya"
+    And user click button name "Lihat Toko Saya"
+    Then user validate text "Brand Tersedia" exist on webview
---
+++ features/android/step_definitions/base_steps.rb
@@ -0,0 +1,34 @@
+  When(/^user click button name "([^"]*)"$/) do |button_name|
+    click_widget_button(button_name)
+  end
+    
+  When(/^user click text view "([^"]*)"$/) do |text_name|
+    click_text_view(text_name)
+  end
+    
+  When(/^validate text "([^"]*)" exist on page$/) do |text_name|
+    sleep 2
+    validate_text(text_name)
+  end
+  
+  And(/^user swipe to bottom of page "([^"]*)" pixel$/) do |pixel|
+    Appium::TouchAction.new.swipe(start_x: 144, start_y: pixel, end_x: 144, end_y: 149).perform
+  end
+  
+  Given("user in onboarding screen") do
+    @basepage = BasePage.new
+    @basepage.is_home_page?
+  end
+  
+  And(/^user input phone number "([^"]*)" on login page$/) do |phone_number|
+    @basepage.input_phone_number(phone_number)
+  end
+  
+  And(/^user input password "([^"]*)" on login page$/) do |password|
+    @basepage.input_password(password)
+  end
+  
+  And(/^user validate text "([^"]*)" exist on webview$/) do |web_text|
+    validate_content_page_webview(web_text)
+  end
+  
